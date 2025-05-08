069de5993e898adb122741b0c007d41aa47c3dd9
Anthony deplanque <anthony.deplanque3184@gmail.com>
2025-05-08 09:07:13 +0200

    fix skins.ts

--- src/constants/skins.ts
+++ src/constants/skins.ts
@@ -1,4 +1,4 @@
-import skins from '../Data/skins.json' assert { type: 'json' };
+import skins from '../data/skins.json' assert { type: 'json' };
 import { Skin } from '../store/slices/skinSlice';
 
 export const allSkins: Skin[] = skins;
