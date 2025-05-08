3e394f62a72b8ff87e782bba6d367bb2f2aa235c
Anthony deplanque <anthony.deplanque3184@gmail.com>
2025-05-08 08:35:31 +0200

    functionnal save game

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
+import { useEffect, useState } from 'react';
 import './styles/App.css';
 import Abyss from './Components/Abyss';
-import { useEffect } from 'react';
-import { loadGame } from './Helpers/save-utils';
 import { useAppDispatch } from './store/hooks';
-import { hydrateAllState } from './store/slices/hydrationSlice';
+import { loadGame } from './Helpers/save-utils';
+import { hydrate as hydrateEssence } from './store/slices/essenceSlice';
+import { hydrate as hydrateCreatures } from './store/slices/creatureSlice';
+import { hydrate as hydrateTentacles } from './store/slices/tentacleSlice';
+import { hydrate as hydrateAutoClickers } from './store/slices/autoClickerSlice';
+import { hydrate as hydrateAnimation } from './store/slices/animationSlice';
+import { hydrate as hydrateCorruption } from './store/slices/corruptionSlice';
+import { hydrate as hydrateSkin } from './store/slices/skinSlice';
+import { hydrate as hydrateDebug } from './store/slices/debugSlice';
+import { setHydrated } from './store/slices/hydrationSlice';
 
 function App() {
   const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const saved = loadGame();
-    if (saved) {
-      console.log('now state hydration', saved);
-      dispatch(hydrateAllState(saved));
-      console.log('hydrate', saved);
+    const game = loadGame();
+    if (game) {
+      if (game.essence) dispatch(hydrateEssence(game.essence));
+      if (game.creatures) dispatch(hydrateCreatures(game.creatures));
+      if (game.tentacles) dispatch(hydrateTentacles(game.tentacles));
+      if (game.autoClicker) dispatch(hydrateAutoClickers(game.autoClicker));
+      if (game.animation) dispatch(hydrateAnimation(game.animation));
+      if (game.corruption) dispatch(hydrateCorruption(game.corruption));
+      if (game.skin) dispatch(hydrateSkin(game.skin));
+      if (game.debug) dispatch(hydrateDebug(game.debug));
     }
+    dispatch(setHydrated(true));
+    setLoading(false);
   }, []);
 
+  if (loading)
+    return <div className="text-white p-4">Chargement abyssal en cours...</div>;
   return <Abyss />;
 }
 
--- src/store/slices/hydrationSlice.ts
+++ src/store/slices/hydrationSlice.ts
@@ -1,58 +1,22 @@
-// src/store/slices/hydrationSlice.ts
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { GlobalState } from '../index';
-import { store } from '../index';
+
+export interface HydrationState {
+  hydrated: boolean;
+}
+
+const initialState: HydrationState = {
+  hydrated: false,
+};
 
 const hydrationSlice = createSlice({
   name: 'hydration',
-  initialState: {},
+  initialState,
   reducers: {
-    hydrateAllState: (_state, action: PayloadAction<Partial<GlobalState>>) => {
-      const payload = action.payload;
-
-      console.log('State Hydration ', payload);
-
-      if (payload.creatures)
-        store.dispatch({
-          type: 'creatures/hydrate',
-          payload: payload.creatures,
-        });
-
-      if (payload.tentacles)
-        store.dispatch({
-          type: 'tentacles/hydrate',
-          payload: payload.tentacles,
-        });
-
-      if (payload.skin)
-        store.dispatch({ type: 'skin/hydrate', payload: payload.skin });
-
-      if (payload.essence)
-        store.dispatch({ type: 'essence/hydrate', payload: payload.essence });
-
-      if (payload.autoClicker)
-        store.dispatch({
-          type: 'autoClicker/hydrate',
-          payload: payload.autoClicker,
-        });
-
-      if (payload.animation)
-        store.dispatch({
-          type: 'animation/hydrate',
-          payload: payload.animation,
-        });
-
-      if (payload.debug)
-        store.dispatch({ type: 'debug/hydrate', payload: payload.debug });
-
-      if (payload.corruption)
-        store.dispatch({
-          type: 'corruption/hydrate',
-          payload: payload.corruption,
-        });
+    setHydrated: (state, action: PayloadAction<boolean>) => {
+      state.hydrated = action.payload;
     },
   },
 });
 
-export const { hydrateAllState } = hydrationSlice.actions;
+export const { setHydrated } = hydrationSlice.actions;
 export default hydrationSlice.reducer;
