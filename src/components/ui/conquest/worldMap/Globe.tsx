f8b77d5e54f52da522bd057c8d2af1048e244521
Anthony deplanque <anthony.deplanque3184@gmail.com>
2025-05-15 08:37:43 +0200

    wip globe

---
+++ src/components/ui/conquest/worldMap/Globe.tsx
@@ -0,0 +1,32 @@
+// Globe.tsx
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+import { useDynamicWorldTexture } from './texture/WorldMapTexture';
+import * as THREE from 'three';
+
+const Globe = () => {
+  const texture = useDynamicWorldTexture();
+
+  return (
+    <Canvas camera={{ position: [0, 0, 7], fov: 50 }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 2, 5]} />
+      <mesh>
+        <sphereGeometry args={[2.02, 64, 64]} />{' '}
+        <meshBasicMaterial color={'#24684d'} side={THREE.BackSide} />
+      </mesh>
+      <mesh>
+        <sphereGeometry args={[2, 64, 64]} />
+        <meshStandardMaterial
+          map={texture}
+          map-offset={[0, 0]}
+          map-repeat={[1, 1]}
+        />
+      </mesh>
+      <Stars radius={500} depth={50} count={5000} factor={4} />
+      <OrbitControls enablePan={true} enableZoom={true} />
+    </Canvas>
+  );
+};
+
+export default Globe;
