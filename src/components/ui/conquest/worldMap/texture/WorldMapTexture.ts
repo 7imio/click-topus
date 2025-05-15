b0d7f09ab2cc22da122dc8e40c77c47dd46af033
Anthony deplanque <anthony.deplanque3184@gmail.com>
2025-05-15 13:50:47 +0200

    functionnal globe

--- src/components/ui/conquest/worldMap/texture/WorldMapTexture.ts
+++ src/components/ui/conquest/worldMap/texture/WorldMapTexture.ts
@@ -1,5 +1,4 @@
-// texture/WorldMapTexture.ts
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import * as THREE from 'three';
 import * as d3 from 'd3-geo';
 import worldMap from '../../../../../data/worldMap.json';
@@ -12,94 +11,88 @@ import {
 } from 'geojson';
 
 const canvasSize = 2048;
-
-const canvasWidth = canvasSize * 1.5;
+const canvasWidth = canvasSize * 1.6;
 const canvasHeight = canvasSize;
-
 const geoData = worldMap as FeatureCollection<Geometry>;
 
 export const useDynamicWorldTexture = () => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const textureRef = useRef<THREE.Texture | null>(null);
-
-  if (!canvasRef.current) {
-    const canvas = document.createElement('canvas');
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-
-    canvasRef.current = canvas;
-
-    textureRef.current = new THREE.CanvasTexture(canvas);
-  }
-
-  const fillWaterColor = (
-    ctx: CanvasRenderingContext2D,
-    centerColor: string,
-    borderColor: string
-  ) => {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    const gradient = ctx.createRadialGradient(
-      canvasWidth / 2,
-      canvasHeight / 2,
-      0,
-      canvasWidth / 2,
-      canvasHeight / 2,
-      canvasWidth / 2
-    );
-
-    gradient.addColorStop(0, centerColor);
-    gradient.addColorStop(1, borderColor);
-
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-  };
-
-  const drawMap = () => {
-    const ctx = canvasRef.current!.getContext('2d')!;
-
-    const centerColor = '#55774F';
-    const borderColor = '#111D29';
-
-    fillWaterColor(ctx, centerColor, borderColor);
-    const countries = getCountriesWithColors();
-
-    const projection = d3.geoNaturalEarth1().fitExtent(
-      [
-        [0, 0],
-        [canvasWidth, canvasHeight],
-      ],
-      geoData
-    );
-
-    const path = d3.geoPath(projection, ctx);
-
-    geoData.features.forEach((feature: Feature<Geometry, GeoJsonProperties>) =>
-      fillCountryColor(feature, countries, ctx, path)
-    );
-    textureRef.current!.needsUpdate = true;
-  };
-
-  const fillCountryColor = (
-    feature: Feature<Geometry, GeoJsonProperties>,
-    countries: any[],
-    ctx: CanvasRenderingContext2D,
-    path: d3.GeoPath<any, d3.GeoPermissibleObjects>
-  ) => {
-    if (!feature.properties) return;
-    const isoCode = feature.properties.iso_a2_eh;
-    const countryData = countries.find((c) => c.ISO_A2 === isoCode);
-
-    if (!countryData) return;
-
-    ctx.fillStyle = countryData.color || '#444';
-    ctx.beginPath();
-    path(feature);
-    ctx.fill();
-  };
+  const [texture, setTexture] = useState<THREE.Texture | null>(null);
 
   useEffect(() => {
-    drawMap();
+    const generateTexture = async () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
+      const ctx = canvas.getContext('2d')!;
+
+      fillWaterColor(ctx, '#55774F', '#111D29');
+      const countries = getCountriesWithColors();
+
+      const projection = d3.geoNaturalEarth1().fitExtent(
+        [
+          [0, 0],
+          [canvasWidth, canvasHeight],
+        ],
+        geoData
+      );
+
+      const path = d3.geoPath(projection, ctx);
+
+      geoData.features.forEach(
+        (feature: Feature<Geometry, GeoJsonProperties>) => {
+          fillCountryColor(feature, countries, ctx, path);
+        }
+      );
+
+      const texture = new THREE.CanvasTexture(canvas);
+      texture.offset.set(0, 0);
+      texture.repeat.set(1, 1);
+      texture.needsUpdate = true;
+
+      setTexture(texture);
+    };
+
+    generateTexture();
   }, []);
 
-  return textureRef.current!;
+  return texture;
+};
+
+const fillWaterColor = (
+  ctx: CanvasRenderingContext2D,
+  centerColor: string,
+  borderColor: string
+) => {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  const gradient = ctx.createRadialGradient(
+    canvasWidth / 2,
+    canvasHeight / 2,
+    0,
+    canvasWidth / 2,
+    canvasHeight / 2,
+    canvasWidth / 2
+  );
+  gradient.addColorStop(0, centerColor);
+  gradient.addColorStop(1, borderColor);
+
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+};
+
+const fillCountryColor = (
+  feature: Feature<Geometry, GeoJsonProperties>,
+  countries: any[],
+  ctx: CanvasRenderingContext2D,
+  path: d3.GeoPath<any, d3.GeoPermissibleObjects>
+) => {
+  if (!feature.properties) return;
+  const isoCode = feature.properties.iso_a2_eh;
+  const countryData = countries.find((c) => c.ISO_A2 === isoCode);
+
+  if (!countryData) return;
+
+  ctx.fillStyle = countryData.color || '#444';
+  ctx.beginPath();
+  path(feature);
+  ctx.fill();
 };
